4eccfdd1854a19bc5177e699bbfca4b1ef9ac5fa
Christian Blank <christian.blank@haw-hamburg.de>
2015-12-09 12:16:39 +0100

    Remove configured logging

--- _config.php
+++ _config.php
@@ -1,9 +1,1 @@
 <?php
-// create adapter
-$logstashAdapter = new LogstashAdapter();
-// create log writer object
-$writer = LogstashFactory::factory($logstashAdapter, NetworkLogWriter::UDP);
-// set formatter
-$writer->setFormatter(new LogstashFormatter());
-// add writer to log mechanism
-SS_Log::add_writer($writer, LogstashFactory::log_level(), '<=');
